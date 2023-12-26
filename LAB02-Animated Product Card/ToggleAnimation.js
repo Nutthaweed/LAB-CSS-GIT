dd54bca08642902ad8ea0764769fcf09e7e520d6
Nutthaweed <97228092+Nutthaweed@users.noreply.github.com>
2023-12-26 15:03:43 +0700

    update

--- LAB02-Animated Product Card/ToggleAnimation.js
+++ LAB02-Animated Product Card/ToggleAnimation.js
@@ -1,11 +1,11 @@
-let circle = document.querySelector(".color-option");
+let circle = document.querySelectorAll(".color-option");
 
 circle.addEventListener("click", (e)=>{
   let target = e.target;
   if(target.classList.contains("circle")){
-    circle.querySelector(".active").classList.remove("active");
+    circle.querySelectorAll(".active").classList.remove("active");
     target.classList.add("active");
-    document.querySelector(".main-images .active").classList.remove("active");
-    document.querySelector(`.main-images .${target.id}`).classList.add("active");
+    document.querySelectorAll(".main-images .active").classList.remove("active");
+    document.querySelectorAll(`.main-images .${target.id}`).classList.add("active");
   }
 });
